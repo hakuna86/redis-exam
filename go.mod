a2dcf28729e88de8d61048068db90ef7e8a78e8a
hakuna <kwangmin010@gmail.com>
2019-11-20 11:42:21 +0900

    git ignore add

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hakuna86/redis-exam
 
 go 1.13
+
+require github.com/go-redis/redis/v7 v7.0.0-beta.4
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-redis/redis/v7"
+)
 
 func main() {
-  fmt.Println("Hello world")
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	defer client.Close()
+
+	pong, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("redis client Ping", pong)
+
+	if err := client.Set("key", "value", 0).Err(); err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err != nil {
+		if err == redis.Nil {
+			fmt.Println("key2 does not exist")
+		} else {
+			panic(err)
+		}
+	}
+	fmt.Println("key2", val2)
 }
