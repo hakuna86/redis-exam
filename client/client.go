bbb9083bde1bef00bd6b15aad04522f3d2a969e9
hakuna <kwangmin010@gmail.com>
2019-11-22 15:56:40 +0900

    ....

--- client/client.go
+++ client/client.go
@@ -40,6 +40,30 @@ func (c *Client) SetKeyMustExist(key string, value interface{}, expiration time.
 	return c.redis.SetXX(key, value, expiration).Result()
 }
 
+func (c *Client) SetMap(key string, value map[string]interface{}) error {
+	return c.redis.HMSet(key, value).Err()
+}
+
+func (c *Client) GetMap(key string) (map[string]string, error) {
+	return c.redis.HGetAll(key).Result()
+}
+
+func (c *Client) SetSlice(key string, value ...interface{}) error {
+	return c.redis.LPush(key, value...).Err()
+}
+
+func (c *Client) GetSlice(key string) ([]string, error) {
+	return c.redis.LRange(key, 0, -1).Result()
+}
+
 func (c *Client) Get(key string) (string, error) {
 	return c.redis.Get(key).Result()
+}
+
+func (c *Client) Publish(channel string, message interface{}) error {
+	return c.redis.Publish(channel, message).Err()
+}
+
+func (c *Client) Subscribe(channel ...string) <-chan *redis.Message {
+	return c.redis.Subscribe(channel...).Channel()
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/go-redis/redis/v7"
 	"github.com/hakuna86/redis-exam/client"
+	"runtime"
 )
 
 type Store struct {
@@ -62,7 +63,19 @@ func SetKeyMustExist(cli *client.Client, keys []Store) {
 	}
 }
 
+func Publish(cli *client.Client, ch, id string) {
+	cnt := 0
+	for {
+		if err := cli.Publish(ch, fmt.Sprintf("%s=%s", id, cnt)); err != nil {
+			fmt.Println("Publish Error", err)
+			return
+		}
+		cnt++
+	}
+}
+
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	cli := client.NewClient()
 	defer cli.Close()
 
@@ -86,5 +99,61 @@ func main() {
 	//SetKeys(cli, keys)
 	//GetKeys(cli, keys)
 	//SetKeysNX(cli, keys)
-	SetKeyMustExist(cli, keys)
+	//SetKeyMustExist(cli, keys)
+
+	_ = keys
+
+	//k := Store{"key-map", map[string]interface{}{
+	//	"one" : 1,
+	//	"two" : 2,
+	//	"three" : 3,
+	//}}
+	//if err := cli.SetMap(k.Key, k.Value.(map[string]interface{})); err != nil {
+	//	fmt.Println("Set Map Error", err)
+	//}
+
+	//v, err := cli.GetMap("key-map")
+	//if err != nil {
+	//	fmt.Println("Set Map Error", err)
+	//}
+	//for key, value := range v {
+	//	fmt.Printf("Get Map Value Key=> %s, Value=> %s \n", key, value)
+	//}
+
+	//sli := Store{"key-array", []string{"1", "2", "3"}}
+	//switch t := sli.Value.(type) {
+	//case []string:
+	//	var data []interface{}
+	//	for _, v := range t {
+	//		data = append(data, v)
+	//	}
+	//	if err := cli.SetSlice(sli.Key, data...); err != nil {
+	//		fmt.Println("Set Slice Error", err)
+	//	}
+	//}
+
+	//res, err := cli.GetSlice("key-array")
+	//if err != nil {
+	//	fmt.Println("Get Slice Error", err)
+	//}
+	//fmt.Println(res)
+
+	//start := time.Now().String()
+	//ch := "test-ch"
+	//go func() {
+	//	for {
+	//		select {
+	//			case msg := <- cli.Subscribe(ch):
+	//				fmt.Println("Receive message", msg.String())
+	//		}
+	//	}
+	//}()
+	//
+	//for i := 0; i < 7; i++ {
+	//	go Publish(cli, ch, fmt.Sprintf("node%d", i+1))
+	//}
+	//
+	//fmt.Println("=========End=========", "start", start, "end", time.Now().String())
+	//<- time.NewTimer(10 * time.Second).C
+
 }
